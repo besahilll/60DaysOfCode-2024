f9f9b400dbb7eaffd4393b35028f2668a89b2206
besahilll <500091837@stu.upes.ac.in>
2024-07-05 23:33:42 +0530

    remove duplicates from sorted array

---
+++ Day1/Remove_Duplicates.java
@@ -0,0 +1,13 @@
+//Remove the duplicates in-place such that each unique element appears only once. 
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int j=1;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]!=nums[i-1]){
+                nums[j]=nums[i];
+                j++;
+            }
+        }
+        return j;
+    }
+}
