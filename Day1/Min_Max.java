4683331952d4b9604d4a5ddc9a851bdfbdb49735
besahilll <500091837@stu.upes.ac.in>
2024-07-06 15:02:46 +0530

    Minimum and maximum element in the array

---
+++ Day1/Min_Max.java
@@ -0,0 +1,18 @@
+/*find the minimum and maximum elements in the array.
+Time complexity: O(n) 
+Space Complexity: O(1) */
+class Solution {
+    public Pair<Long, Long> getMinMax(int[] arr) {
+        int min=Integer.MAX_VALUE;
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]<min){
+                min=arr[i];
+            }
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+        return new Pair<>((long)min,(long)max);
+    }
+}
