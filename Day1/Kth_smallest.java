99e54406f718ca1e1c53002a19e2e996e7f6699f
besahilll <500091837@stu.upes.ac.in>
2024-07-06 23:16:57 +0530

    Kth smallest element in an array

--- Day1/Kth_smallest.java
+++ Day1/Kth_smallest.java
@@ -1,6 +1,6 @@
-.//find the kth smallest element in the given array.
+/*find the kth smallest element in the given array.
 Time Complexity: O(nlogn)
-Space Complexity: O(logn)
+Space Complexity: O(1) */
 class Solution{
     public static int kthSmallest(int[] arr, int l, int r, int k) 
     { 
