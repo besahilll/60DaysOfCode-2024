d7c1606381eda909bfacb6b371cb548f2d784b7d
besahilll <500091837@stu.upes.ac.in>
2024-07-08 17:29:36 +0530

    get kth smallest element in array using Heap

--- Day1/Kth_smallest_optimized.java
+++ Day1/Kth_smallest_optimized.java
@@ -4,13 +4,13 @@ Space Complexity: O(1)*/
 class Solution{
     public static int kthSmallest(int[] arr, int l, int r, int k) 
     { 
-        PriorityQueue<Integer> maxh = new PriorityQueue<>(Collections.reverseOrder()); //defining max-heap
-        for(int i=l;i<=r;i++){
-            maxh.add(arr[i]);
-            if(maxh.size()>k){
-                maxh.poll();
-            }
-        }
-        return maxh.peek();
+                                                                                        PriorityQueue<Integer> maxh = new PriorityQueue<>(Collections.reverseOrder()); //defining max-heap
+                                                                                        for(int i=l;i<=r;i++){
+                                                                                            maxh.add(arr[i]);
+                                                                                            if(maxh.size()>k){
+                                                                                                maxh.poll();
+                                                                                            }
+                                                                                        }
+                                                                                        return maxh.peek();
     } 
 }
